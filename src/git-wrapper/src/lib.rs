4f5da293ce8ec6e84cf0d9ec5c5d33b130af8fc1
ArshiAAkhavan <letmemakenewone@gmail.com>
2025-03-31 21:10:58 -0400

    git wrapper first iteration of implementation

---
+++ src/git-wrapper/src/lib.rs
@@ -0,0 +1,20 @@
+mod error;
+mod wrapper;
+
+use pyo3::prelude::*;
+
+pub use error::Result;
+pub use error::GitError;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn git_wrapper(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
