cec86f6559ee57f393d51d8bf26280882791239f
ArshiAAkhavan <letmemakenewone@gmail.com>
2025-04-03 19:22:25 -0400

    prefix dummy wrappers with Dummy

--- src/anchor/anchor.py
+++ src/anchor/anchor.py
@@ -24,8 +24,8 @@ class GitAnchor:
         """
         self.agent = Agent(api_key)
         self.git_wrapper = GitWrapper(git_repo_link)
-        self.issue_wrapper = IssueWrapper(issue_link)
-        self.code_wrapper = CodeWrapper()
+        self.issue_wrapper = DummyIssueWrapper(issue_link)
+        self.code_wrapper = DummyCodeWrapper()
 
         self.tools = []
 
@@ -50,13 +50,13 @@ class GitAnchor:
                 return getattr(wrapper, name)
 
 
-class IssueWrapper:
+class DummyIssueWrapper:
     def __init__(self, issue_link: str):
         self.issue_link = issue_link
 
     def get_issue_title(self) -> str:
         return "'pkgutil.get_loader' is removed from Python 3.14"
 
-class CodeWrapper:
+class DummyCodeWrapper:
     def __init__(self):
         pass
