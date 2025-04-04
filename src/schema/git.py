4da564366793eedf39e21d26c3487348277e038f
ArshiAAkhavan <letmemakenewone@gmail.com>
2025-04-03 20:19:32 -0400

    Add Descriptions for tools

--- src/schema/git.py
+++ src/schema/git.py
@@ -1,6 +1,6 @@
 from typing import List
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from git_wrapper import Author, CommitMeta, AuthorQuery, Pagination as wrapperPagination
 from anchor.anchor import GitAnchor
 
@@ -11,21 +11,29 @@ class AuthorQueryType(str, Enum):
 
 
 class Pagination(BaseModel):
-    offset: int
-    limit: int
+    """pagination for listing commits"""
+
+    offset: int = Field(..., description="offset starts from 0")
+    limit: int = Field(..., description="limit of number of items to return")
 
     def to_wrapper_pagination(self) -> wrapperPagination:
         return wrapperPagination(offset=self.offset, limit=self.limit)
 
 
 class ListBranches(BaseModel):
+    """listing all branches in a Git repository"""
+
     def __call__(self, anchor: GitAnchor) -> List[str]:
         return anchor.list_branches()
 
 
 class CommitsOfBranch(BaseModel):
-    branch: str
-    pagination: Pagination
+    """listing all commits in a branch"""
+
+    branch: str = Field(..., description="branch name")
+    pagination: Pagination = Field(
+        ..., description="pagination from offset to atleast offset + limit"
+    )
 
     def __call__(self, anchor: GitAnchor) -> List[CommitMeta]:
         return anchor.commits_of_branch(
@@ -34,17 +42,25 @@ class CommitsOfBranch(BaseModel):
 
 
 class AuthorsOfBranch(BaseModel):
-    branch: str
+    """listing all authors in a branch"""
+
+    branch: str = Field(..., description="branch name")
 
     def __call__(self, anchor: GitAnchor) -> List[Author]:
         return anchor.authors_of_branch(self.branch)
 
 
 class CommitsOfAuthor(BaseModel):
-    branch: str
-    query_type: AuthorQueryType
-    query: str
-    pagination: Pagination
+    """listing all commits of an author in a branch"""
+
+    branch: str = Field(..., description="branch name")
+    query_type: AuthorQueryType = Field(
+        ..., description="weather search by name or email"
+    )
+    query: str = Field(..., description="author's name or email")
+    pagination: Pagination = Field(
+        ..., description="pagination from offset to atleast offset + limit"
+    )
 
     def __call__(self, anchor: GitAnchor) -> List[CommitMeta]:
         if self.query_type == AuthorQueryType.NAME:
@@ -58,9 +74,13 @@ class CommitsOfAuthor(BaseModel):
 
 
 class CommitsOnFile(BaseModel):
-    branch: str
-    file_path: str
-    pagination: Pagination
+    """listing all commits on a file in a branch"""
+
+    branch: str = Field(..., description="branch name")
+    file_path: str = Field(..., description="file path")
+    pagination: Pagination = Field(
+        ..., description="pagination from offset to atleast offset + limit"
+    )
 
     def __call__(self, anchor: GitAnchor) -> List[CommitMeta]:
         return anchor.commits_on_file(
@@ -69,10 +89,14 @@ class CommitsOnFile(BaseModel):
 
 
 class CommitsBetween(BaseModel):
-    branch: str
-    start_date: str
-    end_date: str
-    pagination: Pagination
+    """listing all commits between two timestamp in a branch"""
+
+    branch: str = Field(..., description="branch name")
+    start_date: str = Field(..., description="start date in 'Y-m-d H:M:S z' format")
+    end_date: str = Field(..., description="end date in 'Y-m-d H:M:S z' format")
+    pagination: Pagination = Field(
+        ..., description="pagination from offset to atleast offset + limit"
+    )
 
     def __call__(self, anchor: GitAnchor) -> List[CommitMeta]:
         return anchor.commits_between(
