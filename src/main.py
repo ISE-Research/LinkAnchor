f88deae473b9b339deb64a5d3e8bd5faeab6f1ef
ArshiAAkhavan <letmemakenewone@gmail.com>
2025-04-18 14:30:40 -0400

    add cli

--- src/main.py
+++ src/main.py
@@ -3,19 +3,58 @@ from schema.git import TOOLS as GIT_TOOLS
 from schema.code import TOOLS as CODE_TOOLS
 from schema.issue import TOOLS as ISSUE_TOOLS
 import logging
+import argparse
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-)
-# silence httpx logging
-logging.getLogger("httpx").setLevel(logging.WARNING)
 
-ga = GitAnchor("https://github.com/pallets/flask/issues/5472", "git@github.com:pallets/flask.git")
+def parse_arguments():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description="Git Anchor - Link issues to commits")
 
-ga.register_tools(GIT_TOOLS)
-ga.register_tools(CODE_TOOLS)
-ga.register_tools(ISSUE_TOOLS)
+    parser.add_argument("--git", help="Link of the git repository", type=str, required=True)
 
-print("##############")
-print(ga.find_link())
+    parser.add_argument("--issue", help="Link of the issue", type=str, required=True)
+
+    parser.add_argument("--debug", help="Enable debug mode", action="store_true")
+
+    parser.add_argument("--interactive", help="Show advanced UI", action="store_true")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+
+    # Configure logging based on debug flag
+    log_level = logging.INFO if args.debug else logging.WARNING
+
+    logging.basicConfig(
+        level=log_level,
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    )
+    # silence httpx logging
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+
+    logger = logging.getLogger(__name__)
+
+    if args.debug:
+        logger.info("Debug mode enabled")
+
+    if args.interactive:
+        logger.info("Interactive mode enabled")
+
+    ga = GitAnchor(args.issue, args.git)
+
+    ga.register_tools(GIT_TOOLS)
+    ga.register_tools(CODE_TOOLS)
+    ga.register_tools(ISSUE_TOOLS)
+
+    logger.info("Finding link between issue and code...")
+    result = ga.find_link()
+    print("##############")
+    print(result)
+
+    return result
+
+
+if __name__ == "__main__":
+    main()
